a007b248a7055a93498dca6c56b5b15ba90e0064
nanayyayaa <jiejueejuju@gmail.com>
2026-06-09 11:14:28 +0700

    first commit

---
+++ searchtree.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class node
+{
+public:
+    string info;
+    node *leftchild;
+    node *rightchild;
+
+    //constructor for node class
+    
